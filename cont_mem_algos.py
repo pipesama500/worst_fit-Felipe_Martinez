b3c804873eb09e97d477a948f750db964335d835
pipesama500 <103373071+pipesama500@users.noreply.github.com>
2025-03-21 22:42:38 -0500

    Update and rename worst_fit.py to cont_mem_algos.py

--- cont_mem_algos.py
+++ cont_mem_algos.py
@@ -1,30 +1,30 @@
-def worst_fit(memoria_trabajo, req, index):
+def worst_fit(mem_avail, req_size, index):
 
     # Si la lista está vacía
-    if not memoria_trabajo:
+    if not mem_avail:
         return None
 
-    n = len(memoria_trabajo)
+    n = len(mem_avail)
     indice_asignado = -1
     max_limite = -1
 
     # Buscar el segmento con el límite más grande que pueda contener la solicitud
     for i in range(index, n):
-        base, limite = memoria_trabajo[i]
-        if limite >= req and limite > max_limite:
+        base, limite = mem_avail[i]
+        if limite >= req_size and limite > max_limite:
             max_limite = limite
             indice_asignado = i
 
     # Si no se encuentra buscar desde el inicio
     if indice_asignado == -1:
         for i in range(index):
-            base, limite = memoria_trabajo[i]
-            if limite >= req and limite > max_limite:
+            base, limite = mem_avail[i]
+            if limite >= req_size and limite > max_limite:
                 max_limite = limite
                 indice_asignado = i
     # Si se encuentra un segmento válido, devolver la base y el índice
     if indice_asignado != -1:
-        base_asignada = memoria_trabajo[indice_asignado][0]
+        base_asignada = mem_avail[indice_asignado][0]
         return base_asignada, indice_asignado
     else:
         return None # No hay suficiente memoria para la asignación
